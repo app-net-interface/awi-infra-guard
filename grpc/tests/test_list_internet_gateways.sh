bd6c95862a0c5f09df53926b9bfa7f3b3fbfcba2
Michał Bernacki <michber575@gmail.com>
2024-08-13 18:29:57 +0200

    error handling for tests

--- grpc/tests/test_list_internet_gateways.sh
+++ grpc/tests/test_list_internet_gateways.sh
@@ -4,8 +4,18 @@ echo "\n-------------Test ListInternetGateways-------------\n"
 
 server="localhost:50052"
 
-response=$(grpc_cli call $server ListInternetGateways "provider: 'aws'" --json_output 2>/dev/null)
+response=$(grpc_cli call $server ListInternetGateways "provider: 'aws'" --timeout=30 --json_output 2>error_file)
 
+# Check for endpoint errors
+if cat error_file | grep -q "Rpc failed with status code"; then
+    error_message=$(cat error_file | sed -n '/error message:/s/.*error message: //p')
+    echo "Error calling ListInternetGateways endpoint:" $error_message
+    rm error_file
+    exit 1
+fi
+rm error_file
+
+# Check if response not empty
 if echo $response | jq -e '. == {}'  > /dev/null; then
     echo "No internet gateways were found"
     exit 1
--- grpc/tests/test_list_vpc.sh
+++ grpc/tests/test_list_vpc.sh
@@ -4,8 +4,18 @@ echo "\n-------------Test ListVPC-------------\n"
 
 server="localhost:50052"
 
-response=$(grpc_cli call $server ListVPC "provider: 'aws'" --json_output 2>/dev/null)
+response=$(grpc_cli call $server ListVPC "provider: 'aws'" --timeout=30 --json_output 2>error_file)
 
+# Check for endpoint errors
+if cat error_file | grep -q "Rpc failed with status code"; then
+    error_message=$(cat error_file | sed -n '/error message:/s/.*error message: //p')
+    echo "Error calling ListVPC endpoint:" $error_message
+    rm error_file
+    exit 1
+fi
+rm error_file
+
+# Check if response not empty
 if echo $response | jq -e '. == {}'  > /dev/null; then
     echo "No instances were found"
     exit 1
